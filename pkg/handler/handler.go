057bd8830ccd9ea3db1adbaefacc205dce570573
Andras Karasz <andras.karasz@logmein.com>
2020-09-02 19:15:39 +0200

    feat: allow cors header authorization

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -55,6 +55,7 @@ func contextLogger(next http.Handler) http.Handler {
 func allowCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Header", "Authorization")
 
 		next.ServeHTTP(w, r)
 	})
