025c9bc1a849ce3b389f2b8461383b6ef1ce5d7a
Andras Karasz <andras.karasz@logmein.com>
2020-09-24 09:26:13 +0200

    feat: ping handler

--- handler/events.go
+++ handler/events.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
@@ -10,7 +11,46 @@ import (
 	"github.com/akarasz/yahtzee/store"
 )
 
+const (
+	writeWait  = 10 * time.Second
+	pongWait   = 30 * time.Second
+	pingPeriod = (pongWait * 8) / 10
+)
+
 var upgrader = websocket.Upgrader{}
+
+func writer(ws *websocket.Conn) {
+	pingTicker := time.NewTicker(pingPeriod)
+	defer func() {
+		pingTicker.Stop()
+		ws.Close()
+	}()
+
+	for {
+		select {
+		case <-pingTicker.C:
+			ws.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := ws.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func reader(ws *websocket.Conn) {
+	defer func() {
+		ws.Close()
+	}()
+	ws.SetReadLimit(512)
+	ws.SetReadDeadline(time.Now().Add(pongWait))
+	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, _, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+	}
+}
 
 func EventsWSHandler(s store.Store) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -20,24 +60,15 @@ func EventsWSHandler(s store.Store) http.Handler {
 			return
 		}
 
-		c, err := upgrader.Upgrade(w, r, nil)
+		ws, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
-			log.WithField("gameID", gameID).Printf("upgrade:", err)
+			if _, ok := err.(websocket.HandshakeError); !ok {
+				log.Println(err)
+			}
 			return
 		}
-		defer c.Close()
-		for {
-			mt, message, err := c.ReadMessage()
-			if err != nil {
-				log.WithField("gameID", gameID).Printf("read:", err)
-				break
-			}
-			log.WithField("gameID", gameID).Printf("recv: %s", message)
-			err = c.WriteMessage(mt, message)
-			if err != nil {
-				log.WithField("gameID", gameID).Println("write:", err)
-				break
-			}
-		}
+
+		go writer(ws)
+		reader(ws)
 	})
 }
