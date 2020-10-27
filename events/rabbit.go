51044b6de0196846cace422c5b297a2b8c5b5d36
Andras Karasz <andras.karasz@logmein.com>
2020-10-27 09:15:41 +0100

    chore: new and close

--- events/rabbit.go
+++ events/rabbit.go
@@ -4,10 +4,33 @@ import (
 	"github.com/streadway/amqp"
 )
 
-type RabbitMQ struct {
+type Rabbit struct {
 	conn *amqp.Connection
+	ch *amqp.Channel
 }
 
-func (r *RabbitMQ) Emit(gameID string, t Type, body interface{}) {
-	amqp.Dial()
+func (r *Rabbit) Close() {
+	r.ch.Close()
+	r.conn.Close()
+}
+
+func NewRabbit(uri string) (*Rabbit, error) {
+	conn, err := amqp.Dial(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Rabbit{
+		conn: conn,
+		ch: ch,
+	}, nil
+}
+
+func (r *Rabbit) Emit(gameID string, t Type, body interface{}) {
+	amqp.Dial("a")
 }
