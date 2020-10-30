91bbda10a4c50ab8b63507be0265861da7c1e411
Andras Karasz <andras.karasz@logmein.com>
2020-10-30 08:19:03 +0100

    feat: logging rabbit

--- events/rabbit.go
+++ events/rabbit.go
@@ -1,12 +1,15 @@
 package events
 
 import (
+	"encoding/json"
+	"log"
+
 	"github.com/streadway/amqp"
 )
 
 type Rabbit struct {
 	conn *amqp.Connection
-	ch *amqp.Channel
+	ch   *amqp.Channel
 }
 
 func (r *Rabbit) Close() {
@@ -27,10 +30,91 @@ func NewRabbit(uri string) (*Rabbit, error) {
 
 	return &Rabbit{
 		conn: conn,
-		ch: ch,
+		ch:   ch,
 	}, nil
 }
 
 func (r *Rabbit) Emit(gameID string, t Type, body interface{}) {
-	amqp.Dial("a")
+	if err := r.exchangeDeclare(gameID); err != nil {
+		return
+	}
+
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return
+	}
+
+	r.ch.Publish(
+		gameID, // exchange
+		"",     // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(jsonBody),
+		})
+}
+
+func (r *Rabbit) Subscribe(gameID string, clientID interface{}) (chan interface{}, error) {
+	if err := r.exchangeDeclare(gameID); err != nil {
+		return nil, err
+	}
+
+	q, err := r.ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		true,  // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.ch.QueueBind(
+		q.Name, // queue name
+		"",     // routing key
+		gameID, // exchange
+		false,
+		nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO create a chan and send all incoming messages to it until unsubscribe happens
+
+	msgs, err := r.ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("%q: %s: %s", clientID, gameID, d.Body)
+		}
+	}()
+
+	return make(chan interface{}), nil
+}
+
+func (r *Rabbit) Unsubscribe(gameID string, clientID interface{}) error {
+	return nil
+}
+
+func (r *Rabbit) exchangeDeclare(gameID string) error {
+	return r.ch.ExchangeDeclare(
+		gameID,   // name
+		"fanout", // type
+		true,     // durable
+		true,     // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
 }
