a10b50d4a1af20aa4a113a715de30774dcaf6cfd
Marton Nagy <nagy90marci@gmail.com>
2021-02-05 20:54:33 +0100

    add default scorers impl

--- score/score.go
+++ score/score.go
@@ -2,136 +2,208 @@ package score
 
 import "github.com/akarasz/yahtzee"
 
-type Score interface {
-	Ones(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int
-	Twos(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int
-	Threes(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int
-	Fours(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int
-	Fives(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int
-	Sixes(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int
-	ThreeOfAKind(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int
-	FourOfAKind(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int
-	FullHouse(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int
-	SmallStraight(dices []int, scoresheet map[yahtzee.Category]int) int
-	LargeStraight(dices []int, scoresheet map[yahtzee.Category]int) int
-	Yahtzee(dices []int, scoresheet map[yahtzee.Category]int) int
-	Chance(dices []int, scoresheet map[yahtzee.Category]int) int
+type Scorer interface {
+	Score() func(dices []int, scoresheet map[yahtzee.Category]int) int
 }
 
-type Default struct{}
+type DefaultOnes struct{}
 
-func (d Default) Ones(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int {
-	return min(occurrences[1]*1, 5*1)
+func (d *DefaultOnes) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		return min(countDice(1, dices), 5*1)
+	}
 }
-func (d Default) Twos(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int {
-	return min(occurrences[2]*2, 5*2)
+
+type DefaultTwos struct{}
+
+func (d *DefaultTwos) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		return min(countDice(2, dices)*2, 5*2)
+	}
 }
-func (d Default) Threes(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int {
-	return min(occurrences[3]*3, 5*3)
+
+type DefaultThrees struct{}
+
+func (d *DefaultThrees) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		return min(countDice(3, dices)*2, 5*3)
+	}
 }
-func (d Default) Fours(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int {
-	return min(occurrences[4]*4, 5*4)
+
+type DefaultFours struct{}
+
+func (d *DefaultFours) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		return min(countDice(4, dices)*2, 5*4)
+	}
 }
-func (d Default) Fives(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int {
-	return min(occurrences[5]*5, 5*5)
+
+type DefaultFives struct{}
+
+func (d *DefaultFives) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		return min(countDice(5, dices)*5, 5*5)
+	}
 }
-func (d Default) Sixes(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int {
-	return min(occurrences[6]*6, 5*6)
+
+type DefaultSix struct{}
+
+func (d *DefaultSix) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		return min(countDice(6, dices)*6, 5*6)
+	}
 }
-func (d Default) ThreeOfAKind(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int {
-	s := 0
-	for k, v := range occurrences {
-		if v >= 3 {
-			s = max(s, 3*k)
+
+type DefaultThreeOfAKind struct{}
+
+func (d *DefaultThreeOfAKind) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		occurrences := map[int]int{}
+		for _, d := range dices {
+			occurrences[d]++
 		}
-	}
-	return s
-}
-func (d Default) FourOfAKind(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int {
-	s := 0
-	for k, v := range occurrences {
-		if v >= 4 {
-			s = 4 * k
-		}
-	}
-	return s
-}
-func (d Default) FullHouse(occurrences map[int]int, scoresheet map[yahtzee.Category]int) int {
-	s := 0
-	three := false
-	two := false
-	for _, v := range occurrences {
-		if !three && v >= 3 {
-			three = true
-			continue
-		}
-		if !two && v >= 2 {
-			two = true
-			continue
-		}
-	}
-
-	if three && two {
-		s = 25
-	}
-	return s
-}
-func (d Default) SmallStraight(dices []int, scoresheet map[yahtzee.Category]int) int {
-	s := 0
-	hit := [6]bool{}
-	for _, d := range dices {
-		hit[d-1] = true
-	}
-
-	if (hit[0] && hit[1] && hit[2] && hit[3]) ||
-		(hit[1] && hit[2] && hit[3] && hit[4]) ||
-		(hit[2] && hit[3] && hit[4] && hit[5]) {
-		s = 30
-	}
-	return s
-}
-func (d Default) LargeStraight(dices []int, scoresheet map[yahtzee.Category]int) int {
-	s := 0
-	hit := [6]bool{}
-	for _, d := range dices {
-		hit[d-1] = true
-	}
-
-	if (hit[0] && hit[1] && hit[2] && hit[3] && hit[4]) ||
-		(hit[1] && hit[2] && hit[3] && hit[4] && hit[5]) {
-		s = 40
-	}
-	return s
-}
-func (d Default) Yahtzee(dices []int, scoresheet map[yahtzee.Category]int) int {
-	s := 0
-	for i := 1; i < 7; i++ {
-		sameCount := 0
-		for j := 0; j < len(dices); j++ {
-			if dices[j] == i {
-				sameCount++
+		s := 0
+		for k, v := range occurrences {
+			if v >= 3 {
+				s = max(s, 3*k)
 			}
 		}
-		if sameCount >= 5 {
-			s = 50
-			break
-		}
+		return s
 	}
-	return s
 }
-func (d Default) Chance(dices []int, scoresheet map[yahtzee.Category]int) int {
-	s := 0
-	for i := 0; i < len(dices); i++ {
-		sum := 0
-		for j, d := range dices {
-			if len(dices) > 5 && j == i {
+
+type DefaultFourOfAKind struct{}
+
+func (d *DefaultFourOfAKind) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		occurrences := map[int]int{}
+		for _, d := range dices {
+			occurrences[d]++
+		}
+		s := 0
+		for k, v := range occurrences {
+			if v >= 4 {
+				s = 4 * k
+			}
+		}
+		return s
+	}
+}
+
+type DefaultFullHouse struct{}
+
+func (d *DefaultFullHouse) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		occurrences := map[int]int{}
+		for _, d := range dices {
+			occurrences[d]++
+		}
+		s := 0
+		three := false
+		two := false
+		for _, v := range occurrences {
+			if !three && v >= 3 {
+				three = true
 				continue
 			}
-			sum += d
+			if !two && v >= 2 {
+				two = true
+				continue
+			}
 		}
-		s = max(s, sum)
+
+		if three && two {
+			s = 25
+		}
+		return s
 	}
-	return s
+}
+
+type DefaultSmallStraight struct{}
+
+func (d *DefaultSmallStraight) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		s := 0
+		hit := [6]bool{}
+		for _, d := range dices {
+			hit[d-1] = true
+		}
+
+		if (hit[0] && hit[1] && hit[2] && hit[3]) ||
+			(hit[1] && hit[2] && hit[3] && hit[4]) ||
+			(hit[2] && hit[3] && hit[4] && hit[5]) {
+			s = 30
+		}
+		return s
+	}
+}
+
+type DefaultLargeStraight struct{}
+
+func (d *DefaultLargeStraight) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		s := 0
+		hit := [6]bool{}
+		for _, d := range dices {
+			hit[d-1] = true
+		}
+
+		if (hit[0] && hit[1] && hit[2] && hit[3] && hit[4]) ||
+			(hit[1] && hit[2] && hit[3] && hit[4] && hit[5]) {
+			s = 40
+		}
+		return s
+	}
+}
+
+type DefaultYahtzee struct{}
+
+func (d *DefaultYahtzee) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		s := 0
+		for i := 1; i < 7; i++ {
+			sameCount := 0
+			for j := 0; j < len(dices); j++ {
+				if dices[j] == i {
+					sameCount++
+				}
+			}
+			if sameCount >= 5 {
+				s = 50
+				break
+			}
+		}
+		return s
+	}
+}
+
+type DefaultChance struct{}
+
+func (d *DefaultChance) Score() func(dices []int, scoresheet map[yahtzee.Category]int) int {
+	return func(dices []int, scoresheet map[yahtzee.Category]int) int {
+		s := 0
+		for i := 0; i < len(dices); i++ {
+			sum := 0
+			for j, d := range dices {
+				if len(dices) > 5 && j == i {
+					continue
+				}
+				sum += d
+			}
+			s = max(s, sum)
+		}
+		return s
+	}
+}
+
+func countDice(value int, dices []int) int {
+	c := 0
+	for _, d := range dices {
+		if value == d {
+			c++
+		}
+	}
+	return c
 }
 
 func min(a, b int) int {
